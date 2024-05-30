b7950896e79c50c8931ad35a60f99c05634f8714
RamonMacedo1910 <ramonmesquitamacedo2222@gmail.com>
2024-05-30 01:40:16 -0300

    29/05/2024

--- cadastro.js
+++ cadastro.js
@@ -14,9 +14,22 @@ function contar_caracteres(){
 
 }
 
-function mostrarSenha(){
+function mostrarSenha2(){
     var inputPass = document.getElementById('campo_senha2')
-    var btnShowPass = document.getElementById('btn-senha')
+    var btnShowPass = document.getElementById('btn-senha2')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    }else{
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+    }
+}
+
+function mostrarSenha3(){
+    var inputPass = document.getElementById('campo_senha3')
+    var btnShowPass = document.getElementById('btn-senha3')
 
     if(inputPass.type === 'password'){
         inputPass.setAttribute('type', 'text')
--- login.js
+++ login.js
@@ -8,3 +8,16 @@ function logar(){
         alert('usuario ou senha incorretos');
      }
 }
+
+function mostrarSenha(){
+   var inputPass = document.getElementById('campo_senha')
+   var btnShowPass = document.getElementById('btn-senha')
+
+   if(inputPass.type === 'password'){
+       inputPass.setAttribute('type', 'text')
+       btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+   }else{
+       inputPass.setAttribute('type', 'password')
+       btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+   }
+}
